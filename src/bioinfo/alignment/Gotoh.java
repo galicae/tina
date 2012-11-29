117b0cdb21ac45e805d00726b04ed2d67bee58d6
seitza <andre_seitz@web.de>
2012-11-29 22:56:23 +0100

    abstract Gotoh implemented

--- src/bioinfo/alignment/Gotoh.java
+++ src/bioinfo/alignment/Gotoh.java
@@ -18,10 +18,11 @@ import java.io.Writer;
 
 public abstract class Gotoh implements Aligner {
 	
-	int gap = 0;
-	int[][] M;
-	int[][] I;
-	int[][] D;
+	private int gapOpen = 0;
+	private int gapExtend = 0;
+	private int[][] M;
+	private int[][] I;
+	private int[][] D;
 	
 	/**
 	 * Constructor initialising scoring by 
@@ -37,8 +38,9 @@ public abstract class Gotoh implements Aligner {
 	 * certain gap-penalty
 	 * @param gap integer defining gap-costs
 	 */
-	public Gotoh(int gap){
-		this.gap = gap;
+	public Gotoh(int gapOpen, int gapExtend){
+		this.gapOpen = gapOpen;
+		this.gapExtend = gapExtend;
 	}
 	
 	@Override
@@ -46,8 +48,8 @@ public abstract class Gotoh implements Aligner {
 		M = new int[sequence1.length()][sequence2.length()];
 		I = new int[sequence1.length()][sequence2.length()];
 		D = new int[sequence1.length()][sequence2.length()];
-		prepareMatrices();
-		calculateMatrices();
+		prepareMatrices(sequence1,sequence2);
+		calculateMatrices(sequence1,sequence2);
 		return traceback();
 	}
 
@@ -63,7 +65,7 @@ public abstract class Gotoh implements Aligner {
 	 * @return Alignment of the two given Alignables
 	 */
 	private Alignment traceback(){
-		//TODO
+		
 		return null;
 	}
 	
@@ -71,16 +73,46 @@ public abstract class Gotoh implements Aligner {
 	 * prepares matrices for global alignment
 	 * 
 	 */
-	private void prepareMatrices(){
-		//TODO
+	private void prepareMatrices(Alignable sequence1, Alignable sequence2){
+		for(int i = 1; i <= sequence1.length(); i++){ //old: hSeq
+			I[i][0] = 0; //old: vGap
+			D[i][0] = Integer.MIN_VALUE; //old: hGap
+			if(i==1){
+				M[i][0] = M[i-1][0]+gapOpen;
+			}else{
+				M[i][0] = M[i-1][0]+gapExtend;
+			}
+		}
+		
+		for(int i = 1; i <= sequence2.length(); i++){ //old: vSeq
+			D[0][i] = 0;
+			if(i==1){
+				M[0][i] = M[0][i-1]+gapOpen;
+			}else{
+				M[0][i] = M[0][i-1]+gapExtend;
+			}
+			I[0][i] = Integer.MIN_VALUE;
+		}
+		D[0][0] = Integer.MIN_VALUE;
+		I[0][0] = Integer.MIN_VALUE;
+		M[0][0] = 0;
 	}
 	
 	/**
 	 * calculates matrices using scoring-function and gap-penalty
 	 * 
 	 */
-	private void calculateMatrices(){
-		//TODO
+	private void calculateMatrices(Alignable sequence1, Alignable sequence2){
+		for(int i = 1; i <= sequence1.length(); i++){
+			for(int j = 1; j <= sequence2.length(); j++){
+				D[i][j] = Math.max(M[i][j-1]+gapOpen, D[i][j-1]+gapExtend);
+				I[i][j] = Math.max(M[i-1][j]+gapOpen,I[i-1][j]+gapExtend);
+				M[i][j] = Math.max(M[i-1][j-1]+score(sequence2.getComp(i-1),sequence1.getComp(j-1)),
+							Math.max(I[i][j],
+							D[i][j]));
+				
+			}
+		}	
 	}
 	
 	/**
