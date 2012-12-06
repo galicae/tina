b1e8bebe7c6d401681a7a66a6bab29e792136154
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2012-12-06 19:06:09 +0100

    train is hereeeee

--- src/bioinfo/alignment/gotoh/LocalSequenceGotoh.java
+++ src/bioinfo/alignment/gotoh/LocalSequenceGotoh.java
@@ -7,79 +7,82 @@ import bioinfo.alignment.SequenceAlignment;
 
 /**
  * 
- * @author andreseitz
- * Local Alignment of two Sequences
+ * @author andreseitz Local Alignment of two Sequences
  */
-public class LocalSequenceGotoh extends Gotoh{
+public class LocalSequenceGotoh extends Gotoh {
 
-	private static final int INIT_VAL = Integer.MIN_VALUE/2;
+	private static final int INIT_VAL = Integer.MIN_VALUE / 2;
 	private int[][] scoringmatrix;
 	Sequence sequence1;
 	Sequence sequence2;
-	
+
 	/**
 	 * 
 	 * @param gapOpen
 	 * @param gapExtend
-	 * @param scoringmatrix 26x26 matrix containing all scoring values plus some empty lines due to faster access
+	 * @param scoringmatrix
+	 *            26x26 matrix containing all scoring values plus some empty
+	 *            lines due to faster access
 	 */
-	public LocalSequenceGotoh(double gapOpen, double gapExtend, int[][] scoringmatrix) {
+	public LocalSequenceGotoh(double gapOpen, double gapExtend,
+			int[][] scoringmatrix) {
 		super(gapOpen, gapExtend);
 		this.scoringmatrix = scoringmatrix;
 	}
-	
-	public LocalSequenceGotoh(double gapOpen, double gapExtend, double[][] scoringmatrix){
-		super(gapOpen,gapExtend);
+
+	public LocalSequenceGotoh(double gapOpen, double gapExtend,
+			double[][] scoringmatrix) {
+		super(gapOpen, gapExtend);
 		this.scoringmatrix = new int[scoringmatrix.length][scoringmatrix[0].length];
-		for(int i = 0; i != scoringmatrix.length; i++){
-			for(int j = 0; j != scoringmatrix[0].length; j++){
-				this.scoringmatrix[i][j] = (int)(Gotoh.FACTOR*scoringmatrix[i][j]);
+		for (int i = 0; i != scoringmatrix.length; i++) {
+			for (int j = 0; j != scoringmatrix[0].length; j++) {
+				this.scoringmatrix[i][j] = (int) (Gotoh.FACTOR * scoringmatrix[i][j]);
 			}
 		}
 	}
-	
+
 	@Override
 	public SequenceAlignment align(Alignable sequence1, Alignable sequence2) {
-		this.M = new int[sequence1.length()+1][sequence2.length()+1];
-		this.I = new int[sequence1.length()+1][sequence2.length()+1];
-		this.D = new int[sequence1.length()+1][sequence2.length()+1];
-		this.sequence1 = (Sequence)sequence1;
-		this.sequence2 = (Sequence)sequence2;
+		this.M = new int[sequence1.length() + 1][sequence2.length() + 1];
+		this.I = new int[sequence1.length() + 1][sequence2.length() + 1];
+		this.D = new int[sequence1.length() + 1][sequence2.length() + 1];
+		this.sequence1 = (Sequence) sequence1;
+		this.sequence2 = (Sequence) sequence2;
 		prepareMatrices();
 		calculateMatrices();
-		return (SequenceAlignment)traceback();
+		return (SequenceAlignment) traceback();
 	}
 
 	@Override
 	public boolean check(Alignment alignment) {
-		SequenceAlignment ali = (SequenceAlignment)alignment;
+		SequenceAlignment ali = (SequenceAlignment) alignment;
 		int score = 0;
 		char[] row0 = ali.getRow(0);
 		char[] row1 = ali.getRow(1);
 		score = 0;
 		int begin = -1;
 		int end = -1;
-		
+
 		int i = 0;
-		while(begin == -1){
-			if(row0[i] != '-' && row1[i] != '-'){
+		while (begin == -1) {
+			if (row0[i] != '-' && row1[i] != '-') {
 				begin = i;
 			}
 			i++;
 		}
-		
-		i = row0.length-1;
-		while(end == -1){
-			if(row0[i] != '-' && row1[i] != '-'){
+
+		i = row0.length - 1;
+		while (end == -1) {
+			if (row0[i] != '-' && row1[i] != '-') {
 				end = i;
 			}
 			i--;
 		}
-		
-		for(i = begin; i <= end; i++){
-			if (row0[i] == '-' || row1[i] == '-'){
+
+		for (i = begin; i <= end; i++) {
+			if (row0[i] == '-' || row1[i] == '-') {
 				score += this.gapOpen;
-				while(i <= end && (row0[i] == '-' || row1[i] == '-')){
+				while (i <= end && (row0[i] == '-' || row1[i] == '-')) {
 					score += this.gapExtend;
 					i++;
 				}
@@ -88,100 +91,111 @@ public class LocalSequenceGotoh extends Gotoh{
 				score += score(row0[i], row1[i]);
 			}
 		}
-		if(1.0d*score/Gotoh.FACTOR == ali.getScore()){
+		if (1.0d * score / Gotoh.FACTOR == ali.getScore()) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * prepares matrices for global alignment
 	 * 
 	 */
 	private void prepareMatrices() {
-		for(int i = 1; i <= sequence1.length(); i++){ //old: hSeq
-			//I[i][0] = 0; //old: vGap
-			D[i][0] = INIT_VAL; //old: hGap
-			//M[i][0] = 0;
+		for (int i = 1; i <= sequence1.length(); i++) { // old: hSeq
+			// I[i][0] = 0; //old: vGap
+			D[i][0] = INIT_VAL; // old: hGap
+			// M[i][0] = 0;
 		}
-		
-		for(int i = 1; i <= sequence2.length(); i++){ //old: vSeq
-			//D[0][i] = 0;
-			//M[0][i] = 0;
+
+		for (int i = 1; i <= sequence2.length(); i++) { // old: vSeq
+			// D[0][i] = 0;
+			// M[0][i] = 0;
 			I[0][i] = INIT_VAL;
 		}
 		D[0][0] = INIT_VAL;
 		I[0][0] = INIT_VAL;
-		//M[0][0] = 0;		
+		// M[0][0] = 0;
 	}
-	
+
 	/**
 	 * calculates matrices using scoring-function and gap-penalty
 	 * 
 	 */
 	private void calculateMatrices() {
-		for(int i = 1; i <= sequence1.length(); i++){
-			for(int j = 1; j <= sequence2.length(); j++){
-				D[i][j] = Math.max(M[i][j-1]+gapOpen+gapExtend, D[i][j-1]+gapExtend);
-				I[i][j] = Math.max(M[i-1][j]+gapOpen+gapExtend,I[i-1][j]+gapExtend);
-				M[i][j] = Math.max(M[i-1][j-1]+score(sequence1.getComp(i-1),sequence2.getComp(j-1)),
-							Math.max(I[i][j],
-							Math.max(D[i][j],0)));
-				
+		int[][] tempScore = new int[sequence1.length()][sequence2.length()];
+		char[] seq1 = ((Sequence) sequence1).getSequence();
+		char[] seq2 = ((Sequence) sequence2).getSequence();
+
+		for (int i = 1; i <= sequence1.length(); i++) {
+			for (int j = 1; j <= sequence2.length(); j++) {
+				tempScore[i - 1][j - 1] = score(seq1[i - 1], seq2[j - 1]);
 			}
-		}		
+		}
+		for (int i = 1; i <= sequence1.length(); i++) {
+			for (int j = 1; j <= sequence2.length(); j++) {
+				D[i][j] = Math.max(M[i][j - 1] + gapOpen + gapExtend,
+						D[i][j - 1] + gapExtend);
+				I[i][j] = Math.max(M[i - 1][j] + gapOpen + gapExtend,
+						I[i - 1][j] + gapExtend);
+				M[i][j] = Math.max(M[i - 1][j - 1] + tempScore[i - 1][j - 1],
+						Math.max(I[i][j], Math.max(D[i][j], 0)));
+
+			}
+		}
 	}
-	
+
 	/**
 	 * Override this method in extensions!
+	 * 
 	 * @return Alignment of the two given Alignables
 	 */
 	private Alignment traceback() {
-		
+
 		int max = INIT_VAL;
-		int x =	0;
+		int x = 0;
 		int y = 0;
-		
-		for(int i = 0; i != M.length; i++){
-			for(int j = 0; j != M[i].length; j++){
-				if(max <= M[i][j]){
+
+		for (int i = 0; i != M.length; i++) {
+			for (int j = 0; j != M[i].length; j++) {
+				if (max <= M[i][j]) {
 					max = M[i][j];
-					x = i-1;
-					y = j-1;
+					x = i - 1;
+					y = j - 1;
 				}
 			}
 		}
-		
+
 		int score = max;
 		String row0 = "";
 		String row1 = "";
 		int actScore = 0;
 		char actx;
 		char acty;
-		
-		for(int i = M[M.length-1].length-1; i > y+1; i--){
+
+		for (int i = M[M.length - 1].length - 1; i > y + 1; i--) {
 			row0 += "-";
-			row1 += sequence2.getComp(i-1);
+			row1 += sequence2.getComp(i - 1);
 		}
-		for(int i = M.length-1; i > x+1; i--){
-			row0 += sequence1.getComp(i-1);
+		for (int i = M.length - 1; i > x + 1; i--) {
+			row0 += sequence1.getComp(i - 1);
 			row1 += "-";
 		}
-		
-		while(x >= 0 && y >= 0 && M[x+1][y+1] != 0){
-			
-			actScore = M[x+1][y+1];
+
+		while (x >= 0 && y >= 0 && M[x + 1][y + 1] != 0) {
+
+			actScore = M[x + 1][y + 1];
 			actx = sequence1.getComp(x);
 			acty = sequence2.getComp(y);
-			
-			if(actScore == M[x][y]+score(actx, acty)){
+
+			if (actScore == M[x][y] + score(actx, acty)) {
 				row0 += actx;
 				row1 += acty;
 				y--;
 				x--;
-			} else if (actScore == D[x+1][y+1]){
-				while(D[x+1][y+1] == D[x+1][y]+gapExtend && y > 0){
+			} else if (actScore == D[x + 1][y + 1]) {
+				while (D[x + 1][y + 1] == D[x + 1][y] + gapExtend && y > 0) {
 					row0 += "-";
 					row1 += acty;
 					y--;
@@ -190,8 +204,8 @@ public class LocalSequenceGotoh extends Gotoh{
 				row0 += "-";
 				row1 += acty;
 				y--;
-			} else if (actScore == I[x+1][y+1]){
-				while(I[x+1][y+1] == I[x][y+1]+gapExtend && x > 0){
+			} else if (actScore == I[x + 1][y + 1]) {
+				while (I[x + 1][y + 1] == I[x][y + 1] + gapExtend && x > 0) {
 					row0 += actx;
 					row1 += "-";
 					x--;
@@ -200,32 +214,35 @@ public class LocalSequenceGotoh extends Gotoh{
 				row0 += actx;
 				row1 += "-";
 				x--;
-			}	
+			}
 		}
-		for(int i = y+1; i > 0; i--){
+		for (int i = y + 1; i > 0; i--) {
 			row0 += "-";
-			row1 += sequence2.getComp(i-1);
+			row1 += sequence2.getComp(i - 1);
 		}
-		for(int i = x+1; i > 0; i--){
-			row0 += sequence1.getComp(i-1);
+		for (int i = x + 1; i > 0; i--) {
+			row0 += sequence1.getComp(i - 1);
 			row1 += "-";
 		}
-		
-		return new SequenceAlignment(sequence1, sequence2, flip(row0.toCharArray()), flip(row1.toCharArray()), 1.0d*score/Gotoh.FACTOR);
+
+		return new SequenceAlignment(sequence1, sequence2,
+				flip(row0.toCharArray()), flip(row1.toCharArray()), 1.0d
+						* score / Gotoh.FACTOR);
 	}
 
 	/**
-	 * @param two components of Alignable implementing equals
+	 * @param two
+	 *            components of Alignable implementing equals
 	 * @return score between two components of Alignable
 	 */
 	private int score(char x, char y) {
-		return scoringmatrix[x-65][y-65];
+		return scoringmatrix[x - 65][y - 65];
 	}
-	
-	private char[] flip(char[] in){
+
+	private char[] flip(char[] in) {
 		char[] out = new char[in.length];
-		for(int i = in.length-1; i >= 0; i --){
-			out[out.length-1-i] = in[i];
+		for (int i = in.length - 1; i >= 0; i--) {
+			out[out.length - 1 - i] = in[i];
 		}
 		return out;
 	}
--- src/test/GotohTest.java
+++ src/test/GotohTest.java
@@ -23,31 +23,30 @@ public class GotohTest {
 	 */
 	public static void main(String[] args) {
 		String seqlibfile = args[1];
-		HashMap<String,String> seqlib = new HashMap<String,String>();
+		HashMap<String, String> seqlib = new HashMap<String, String>();
 		BufferedReader br = null;
 		String line = null;
 		String[] content;
 		String infile = args[0];
-		
-		try{
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(seqlibfile)));
-		}catch(Exception e){
+
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(
+					seqlibfile)));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(br == null){
+		if (br == null) {
 			return;
 		}
-		try{
-			while((line = br.readLine()) != null){
+		try {
+			while ((line = br.readLine()) != null) {
 				content = line.split(":");
-				seqlib.put(content[0].trim(),content[1].trim());
+				seqlib.put(content[0].trim(), content[1].trim());
 			}
 			br.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
 
 		Sequence seq1;
 		Sequence seq2;
@@ -55,37 +54,53 @@ public class GotohTest {
 		String ali2;
 		SequenceAlignment ali;
 		double[][] matrix = QuasarMatrix.parseMatrix(args[2]);
-		GlobalSequenceGotoh gotoh = new GlobalSequenceGotoh(-12.0d,-1.0d,matrix);
-		try{
-			br = new BufferedReader(new InputStreamReader(new FileInputStream(infile)));
-		}catch(Exception e){
+		FreeshiftSequenceGotoh gotoh = new FreeshiftSequenceGotoh(-12.0d, -1.0d,
+				matrix);
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(
+					infile)));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(br == null){
+		if (br == null) {
 			return;
 		}
-		try{
-			while((line = br.readLine()) != null){
+		try {
+			long start = System.currentTimeMillis();
+			while ((line = br.readLine()) != null) {
 				ali1 = br.readLine();
 				ali2 = br.readLine();
-				line = line.substring(1);
+				if(line.startsWith(">"))
+					line = line.substring(1);
 				content = line.split("\\s");
-				seq1 = new Sequence(content[0].trim(),seqlib.get(content[0].trim()));
-				seq2 = new Sequence(content[1].trim(),seqlib.get(content[1].trim()));
+//				System.out.println((content[0]));
+//				System.out.println((content[0].trim()));
+//				System.out.println(seqlib.get(content[0].trim()));
+				seq1 = new Sequence(content[0].trim(), seqlib.get(content[0]
+						.trim()));
+				seq2 = new Sequence(content[1].trim(), seqlib.get(content[1]
+						.trim()));
 				ali = gotoh.align(seq1, seq2);
-				//gotoh.check(ali);
-				System.out.println(">"+line+" "+String.format(Locale.US,"%.3f",ali.getScore()));
-				System.out.println(ali1);
-				System.out.println(ali2);
-				System.out.println(content[0].trim()+": "+ali.getRowAsString(0));
-				System.out.println(content[1].trim()+": "+ali.getRowAsString(1));
-				//gotoh.streamMatricesAsHtml(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/Users/andreseitz/Desktop/test.ali"))));
+				// gotoh.check(ali);
+//				System.out.println(">" + line + " "
+//						+ String.format(Locale.US, "%.3f", ali.getScore()));
+//				System.out.println(ali1);
+//				System.out.println(ali2);
+//				System.out.println(content[0].trim() + ": "
+//						+ ali.getRowAsString(0));
+//				System.out.println(content[1].trim() + ": "
+//						+ ali.getRowAsString(1));
+				// gotoh.streamMatricesAsHtml(new BufferedWriter(new
+				// OutputStreamWriter(new
+				// FileOutputStream("/Users/andreseitz/Desktop/test.ali"))));
 			}
 			br.close();
-		}catch(Exception e){
+			long end = System.currentTimeMillis();
+			System.out.println(end - start);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
