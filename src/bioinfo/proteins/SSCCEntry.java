0e8c910cd261bc703e8633955856c6880e3460bf
seitza <andre_seitz@web.de>
2012-12-07 15:40:29 +0100

    sscc initialised

---
+++ src/bioinfo/proteins/SSCCEntry.java
@@ -0,0 +1,30 @@
+package bioinfo.proteins;
+
+import bioinfo.alignment.Alignable;
+
+public class SSCCEntry implements Alignable{
+
+	private SSCCLine[] lines;
+	private int length;
+	private final String id;
+	
+
+	@Override
+	public Object getComp(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/bioinfo/proteins/SSCCLine.java
@@ -0,0 +1,13 @@
+package bioinfo.proteins;
+
+public class SSCCLine {
+	
+	private final AminoAcidName name;
+	private char secStruct;
+	private int localContacts;
+	private int globalContacts;
+
+
+	
+
+}
