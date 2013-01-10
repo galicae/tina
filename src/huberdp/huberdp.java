73bac3886e08b0ceb27ffa532637dff5b402a418
Stefan Huber <highqling@gmail.com>
2013-01-10 13:02:19 +0100

    just added files for hubeRDP and the RDP interface. Nothing done up to now.

---
+++ src/huberdp/huberdp.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * hubeRDP is a RDP implementation for the GoBi 2012/13.                      *
+ *                                                                            *
+ * This file is best read at line width 80 and tab width 4.                   *
+ *                                                                   huberste *
+ ******************************************************************************/
+package huberdp;
+
+/**
+ * @author huberste
+ * @lastchange 2013-01-10
+ */
+public class huberdp {
+
+}
+
+/******************************************************************************
+ * "A question that sometimes drives me hazy:                                 *
+ *  Am I or are the others crazy?"                                            *
+ *     - Albert Einstein (1979 - 1966)                                        *
+ ******************************************************************************/
---
+++ src/test/huberdptest.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * hubeRDPTest is mainly a test routine for calling hubeRDP.                  *
+ *                                                                            *
+ * This file is best read at line width 80 and tab width 4.                   *
+ *                                                                   huberste *
+ ******************************************************************************/
+package test;
+
+/**
+ * @author huberste
+ * @lastchange 2013-01-10
+ * 
+ */
+public class huberdptest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/******************************************************************************
+ * "A question that sometimes drives me hazy:                                 *
+ *  Am I or are the others crazy?"                                            *
+ *     - Albert Einstein (1979 - 1966)                                        *
+ ******************************************************************************/
