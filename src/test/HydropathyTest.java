4e15addb630689e78a6c1a86cc3de630f4c374d2
Hischmifisch <Hischmifisch@gmx.de>
2013-01-16 18:58:08 +0100

    fitted secStructMatrix, benchmark will start now

--- src/test/HydropathyTest.java
+++ src/test/HydropathyTest.java
@@ -5,9 +5,9 @@ import bioinfo.alignment.kerbsch.InitClass;
 public class HydropathyTest {
 	public static void main(String[] args) throws ClassNotFoundException {
 		InitClass bla = new InitClass();
-		double[] test = bla.calcPolarityScores();
+		double[] test = bla.calcSecStructScores();
 		double[][] matrix = bla.calcGotohInputMatrix(test);
 //		System.out.println(matrix.length);
-		bla.printMatrix(matrix, test, "polarity");
+		bla.printMatrix(matrix, test, "secstruct");
 	}
 }
