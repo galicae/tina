2ed2779caaba2f57a3b5436c19ac43552bfc5f74
galicae <galicae@galicae-VPCEA2S1E.(none)>
2013-02-12 11:05:15 +0100

    reprof working example

--- src/test/ReprofTest.java
+++ src/test/ReprofTest.java
@@ -6,33 +6,33 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class ReprofTest {
-	
+
 	public static void main(String[] args) throws Exception {
-		reprof("lib/reprof", "~/Desktop/example.fasta", "/home/galicae/Desktop");
-		
+		reprof("lib/reprof", "/home/galicae/Desktop/example.fasta",
+				"/home/galicae/Desktop");
+
 	}
-	
-	public static void reprof(String reprofDir, String input, String output) throws Exception {
-		if(reprofDir.endsWith("/"))
-			reprofDir = reprofDir.substring(0, reprofDir.length()-1);
-		
-		String model = " --model " + reprofDir + "/share/";
-		input = " --input " + input;
-//		output = " --output " + output;
-//		input = "";
-		output = "";
-		model = "";
-		
-		String call = reprofDir + "/bin/reprof" + input + output + model;
-		System.out.println(call);
-		Process process = new ProcessBuilder(call).start();
+
+	public static void reprof(String reprofDir, String input, String output)
+			throws Exception {
+		if (reprofDir.endsWith("/"))
+			reprofDir = reprofDir.substring(0, reprofDir.length() - 1);
+
+		String model = reprofDir + "/reprof/share";
+//		input = "--input" + input;
+		output = "--output" + output;
+
+		String call = reprofDir + "/bin/reprof";
+		// , input, output, model, " --single"
+		System.out.println("");
+		Process process = new ProcessBuilder(call, "--input", input, "--output", output, "--model", model, "--single").start();
 		InputStream is = process.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		String line;
 
 		while ((line = br.readLine()) != null) {
-		  System.out.println(line);
+			System.out.println(line);
 		}
 
 	}
