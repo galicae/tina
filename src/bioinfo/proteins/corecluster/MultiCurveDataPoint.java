cab03f3824a1fe9b60e68d82a843f427f20ed03b
galicae <galicae@galicae-VPCEA2S1E.(none)>
2013-02-26 11:36:17 +0100

    blabla

--- src/bioinfo/proteins/corecluster/MultiCurveDataPoint.java
+++ src/bioinfo/proteins/corecluster/MultiCurveDataPoint.java
@@ -7,45 +7,50 @@ public class MultiCurveDataPoint {
 	private double voronoiArea;
 	private double theta;
 	private String type;
-
+	public int minLength;
+	
+	
 	public MultiCurveDataPoint(String id, double voronoiArea, double theta,
-			String type) {
+			String type, int minLength) {
+		super();
 		this.id = id;
 		this.voronoiArea = voronoiArea;
-		this.theta = theta / Math.PI * 180;
+		this.theta = theta * 180.0 / Math.PI;
 		this.type = type;
+		this.minLength = minLength;
 	}
-
+	
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public double getVoronoiArea() {
 		return voronoiArea;
 	}
-
 	public void setVoronoiArea(double voronoiArea) {
 		this.voronoiArea = voronoiArea;
 	}
-
 	public double getTheta() {
 		return theta;
 	}
-
 	public void setTheta(double theta) {
 		this.theta = theta;
 	}
-
 	public String getType() {
 		return type;
 	}
-
 	public void setType(String type) {
 		this.type = type;
 	}
+	public int getMinLength() {
+		return minLength;
+	}
+	public void setMinLength(int minLength) {
+		this.minLength = minLength;
+	}
+
+	
 
 }
